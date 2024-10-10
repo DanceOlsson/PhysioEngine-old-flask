bc7868a860bbcdebf4801bcaedd352ea9e8a9aef
Alexander Olsson <danceolsson@gmail.com>
2024-10-10 16:15:27 +0200

    little text change on landing page

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    env = os.environ.get('FLASK_ENV', 'development')
+    port = int(os.environ.get('PORT', 8000))
+    debug = env == 'development'
+    app.run(host='0.0.0.0', port=port, debug=debug)
