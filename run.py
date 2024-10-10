36c3d428e59a14df6292fa0028383eff779f77bc
Alexander Olsson <danceolsson@gmail.com>
2024-10-10 16:48:46 +0200

    Improve app configuration and security settings

--- run.py
+++ run.py
@@ -4,7 +4,6 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    env = os.environ.get('FLASK_ENV', 'development')
-    port = int(os.environ.get('PORT', 8000))
-    debug = env == 'development'
+    port = int(os.environ.get("PORT", 8000))
+    debug = os.environ.get("FLASK_DEBUG", "0") == "1"
     app.run(host='0.0.0.0', port=port, debug=debug)
