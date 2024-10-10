3a397cfd0ee377fa5dfb16c3432e5b63210b795f
Alexander Olsson <danceolsson@gmail.com>
2024-10-10 14:27:20 +0200

    update procfil and run.py for heroku

--- app/__init__.py
+++ app/__init__.py
@@ -9,22 +9,4 @@ from config import Config
 
 def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object(config_class)
-
-    # Initialize extensions
-    Talisman(app, content_security_policy=app.config['CSP'], force_https=True)
-    app.wsgi_app = WhiteNoise(app.wsgi_app, root='app/static/', prefix='static/')
-
-    # Register blueprints
-    from app.routes import main, physio, user
-    app.register_blueprint(main.bp)
-    app.register_blueprint(physio.bp, url_prefix='/physio')
-    app.register_blueprint(user.bp, url_prefix='/user')
-
-    # Context Processor to inject current year into all templates
-    @app.context_processor
-    def inject_current_year():
-        from datetime import datetime
-        return {'current_year': datetime.now().year}
-
     return app
--- run.py
+++ run.py
@@ -1,11 +1,11 @@
-from app import create_app
 import os
+from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    if os.environ.get('HEROKU'):
-        port = int(os.environ.get('PORT', 5000))
-        app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    if os.environ.get('FLASK_ENV') == 'development':
+        app.run(debug=True, port=port)
     else:
-        app.run(debug=True, port=8000)
+        app.run(host='0.0.0.0', port=port)
